2cbb06f8d7a7556b9a446ca86042254b6a214f59
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-03-20 21:51:32 -0400

    made question class and its object

---
+++ question_model.py
@@ -0,0 +1,8 @@
+class Question():
+    def __init__(self, input_text, input_answer):
+        self.text = input_text
+        self.answer = input_answer
+
+question1 = Question("5+3","8")
+
+print(question1.answer)
